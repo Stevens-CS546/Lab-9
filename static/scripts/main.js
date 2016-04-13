d5cd0a5d158cf434885885436b9bfac848d28c2e
Phil Barresi <me@philbarresi.com>
2016-04-12 22:25:49 -0400

    Updated lab because I accidentally saved the solution last time

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,6 @@
 (function($) {
     $("nav a").on("click", function(event) {
         var anchor = $(this);
-        event.preventDefault();
 
         var target = anchor.attr("href");
         var chapterTarget = $(target);
